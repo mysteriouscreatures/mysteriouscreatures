efd02f0c157880ab35f930885742041210539154
mysteriouscreatures <55229812+mysteriouscreatures@users.noreply.github.com>
2019-09-19 11:12:00 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -11,3 +11,22 @@ function showselectblock(a){
 		secondselectblock.classList.remove("hidden");
 	}
 }
+
+var burger = document.getElementById("burger");
+var mobmenu = document.getElementById("mobmenu");
+var isburger = false;
+
+function burger_menu(){
+	if (!isburger) {
+		mobmenu.classList.add("menuopened");
+		burger.classList.remove("fa-bars");
+		burger.classList.add("fa-times");
+		isburger = true;
+	}
+	else if (isburger){
+		mobmenu.classList.remove("menuopened");
+		burger.classList.add("fa-bars");
+		burger.classList.remove("fa-times");
+		isburger = false;
+	}
+}
