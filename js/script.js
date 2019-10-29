6c0ea546e8043374a602af5abb7d3745bcef633a
mysteriouscreatures <55229812+mysteriouscreatures@users.noreply.github.com>
2019-10-29 15:10:19 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,16 +1,50 @@
 var firstselectblock = document.getElementById("first-select-block");
 var secondselectblock = document.getElementById("second-select-block");
+var firstquiz = document.getElementById("firstquiz");
+var secondquiz = document.getElementById("secondquiz");
+var finalquiz = document.getElementById("finalquiz");
 
 function showselectblock(a){
 	if(a=="1"){
 		firstselectblock.classList.remove("hidden");
 		secondselectblock.classList.add("hidden");
+		secondquiz.classList.add("hidden");
+		firstquiz.classList.add("hidden");
 	}
 	else if(a=="2"){
 		firstselectblock.classList.add("hidden");
 		secondselectblock.classList.remove("hidden");
+		secondquiz.classList.add("hidden");
+		firstquiz.classList.add("hidden");
 	}
 }
+
+
+function showquiz(a){
+	if(a=="1"){
+		firstquiz.classList.remove("hidden");
+		secondquiz.classList.add("hidden");
+		firstselectblock.classList.add("hidden");
+		secondselectblock.classList.add("hidden");
+		finalquiz.classList.add("hidden");
+	}
+	else if(a=="2"){
+		firstquiz.classList.add("hidden");
+		secondquiz.classList.remove("hidden");
+		firstselectblock.classList.add("hidden");
+		secondselectblock.classList.add("hidden");
+		finalquiz.classList.add("hidden");
+	}
+	else if(a=="3"){
+		firstquiz.classList.add("hidden");
+		secondquiz.classList.add("hidden");
+		finalquiz.classList.remove("hidden");
+		firstselectblock.classList.add("hidden");
+		secondselectblock.classList.add("hidden");
+
+	}
+}
+
 
 var burger = document.getElementById("burger");
 var mobmenu = document.getElementById("mobmenu");
@@ -30,3 +64,4 @@ function burger_menu(){
 		isburger = false;
 	}
 }
+
